785c506ae1a1203b3ba570c3c356ca8040dab0cc
Yulianismelendez <yulianis.melendez@cecar.edu.co>
2023-10-13 17:43:32 -0500

    ejemplos nuevos

--- js/consumirapipaises.js
+++ js/consumirapipaises.js
@@ -5,11 +5,12 @@ function poblarDatosPaises() {
         .then(paises => {
 
             // crearListaPaises(paises)
-            adicionarDatosTablaPaises(paises)
+            adicionarDatosTablaPaises(paises);
+            agregarElementosSelect(paises);
 
         });
 
-    /*function adicionarDatosTablaPaises(paises) {
+    function adicionarDatosTablaPaises(paises) {
         var tabla = document.getElementById("tablaPaises")
 
         for (const pais of paises) {
@@ -23,9 +24,9 @@ function poblarDatosPaises() {
             columnaRegion.innerHTML = pais.region
         }
 
-    }*/
+    }
 
-    function adicionarDatosTablaPaises(paises) {
+    /*function adicionarDatosTablaPaises(paises) {
         var tabla = document.getElementById("tablaPaisesBonificacion")
 
         for (const pais of paises) {
@@ -43,6 +44,47 @@ function poblarDatosPaises() {
             columnaPoblacion.innerHTML = pais.population
         }
 
+    }*/
+
+    function agregarElementosSelect(paises) {
+
+        var selectPaises = document.getElementById("selectPaises");
+
+        for (const pais of paises) {
+
+            var option = crearNodoTexto('option',pais.name.official);
+            adicionarNodoContenedor(selectPaises,option);
+            
+        }
+
     }
+
+}
+
+function mostrarDatosPaisSeleccionado(paisBuscar){
+
+    console.log(paisBuscar);
+
+    var tabla = document.getElementById("tablaPaises");
+
+    tabla.remove();
+
+   for (const pais of paises) {
+
+    if (paisBuscar == pais.name.common) {
+
+        var fila = tabla.insertRow(-1)
+        var columnaNombre = fila.insertCell(0)
+        var columnaCapital = fila.insertCell(1)
+        var columnaRegion = fila.insertCell(2)
+
+        columnaNombre.innerHTML = pais.name.official
+        columnaCapital.innerHTML = pais.capital[0]
+        columnaRegion.innerHTML = pais.region
+        
+    }
+
+    
+   }
 
 }
