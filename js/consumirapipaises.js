49362b59963f28401a08d88e5c767287076e1ecf
Yulianismelendez <yulianis.melendez@cecar.edu.co>
2023-11-02 21:54:51 -0500

    cambios js

--- js/consumirapipaises.js
+++ js/consumirapipaises.js
@@ -1,90 +1,86 @@
-function poblarDatosPaises() {
-    var url = 'https://restcountries.com/v3.1/independent?status=true'
-    fetch(url)
-        .then(respuesta => respuesta.json())
-        .then(paises => {
+var respuestaPaisesAPI;
 
-            // crearListaPaises(paises)
+function poblarDatosPaises() {
+    var url = "https://restcountries.com/v3.1/independent?status=true";
+
+    fetch(url)
+        .then(respuesta => {
+            if (!respuesta.ok) {
+                throw new Error('Network response was not ok');
+            }
+            return respuesta.json();
+        })
+        .then(paises => {
+            respuestaPaisesAPI = paises; // Guarda la respuesta de la API en una variable global
             adicionarDatosTablaPaises(paises);
             agregarElementosSelect(paises);
-
+        })
+        .catch(error => {
+            console.error('Error:', error);
         });
 
-    function adicionarDatosTablaPaises(paises) {
-        var tabla = document.getElementById("tablaPaises")
-
-        for (const pais of paises) {
-            var fila = tabla.insertRow(-1)
-            var columnaNombre = fila.insertCell(0)
-            var columnaCapital = fila.insertCell(1)
-            var columnaRegion = fila.insertCell(2)
-
-            columnaNombre.innerHTML = pais.name.official
-            columnaCapital.innerHTML = pais.capital[0]
-            columnaRegion.innerHTML = pais.region
+        function adicionarDatosTablaPaises(paises) {
+            var tabla = document.getElementById("tablaPaises");
+            tabla.innerHTML = ""; // Limpia la tabla antes de agregar nuevos datos
+            for (const pais of paises) {
+                var fila = tabla.insertRow(-1);
+                var colomnaNombre = fila.insertCell(0);
+                var colomnaCapital = fila.insertCell(1);
+                var colomnaRegion = fila.insertCell(2);
+                var columnacurrencies = fila.insertCell(3);
+                var colomnaBanderas = fila.insertCell(4);
+                var colomnaPoblacion = fila.insertCell(5);
+        
+                colomnaNombre.innerHTML = pais.name.common; // Ajusta el campo del nombre
+                colomnaCapital.innerHTML = pais.capital[0];
+                colomnaRegion.innerHTML = pais.region;
+                columnacurrencies.innerHTML = obtenerNombresMonedas(pais.currencies);
+                colomnaBanderas.innerHTML = `<img src="${pais.flags.png}" alt="Bandera de ${pais.name.common}" width="50">`; // Ajusta el campo de la bandera
+                colomnaPoblacion.innerHTML = pais.population;
+            }
         }
 
-    }
-
-    /*function adicionarDatosTablaPaises(paises) {
-        var tabla = document.getElementById("tablaPaisesBonificacion")
-
-        for (const pais of paises) {
-            var fila = tabla.insertRow(-1)
-            var columnaNombre = fila.insertCell(0)
-            var columnaCapitales = fila.insertCell(1)
-            var columnaMonedas = fila.insertCell(2)
-            var columnaBandera = fila.insertCell(3)
-            var columnaPoblacion = fila.insertCell(4)
-
-            columnaNombre.innerHTML = pais.name.official
-            columnaCapitales.innerHTML = pais.capital[0]
-            columnaMonedas.innerHTML = pais.currencies
-            columnaBandera.innerHTML = pais.region
-            columnaPoblacion.innerHTML = pais.population
+        function obtenerNombresMonedas(currencies) {
+            const nombres = [];
+            for (const codigoMoneda in currencies) {
+                nombres.push(currencies[codigoMoneda].name);
+            }
+            return nombres.join(', ');
         }
-
-    }*/
 
     function agregarElementosSelect(paises) {
-
         var selectPaises = document.getElementById("selectPaises");
+        selectPaises.innerHTML = ""; // Limpia el select antes de agregar nuevos elementos
 
         for (const pais of paises) {
-
-            var option = crearNodoTexto('option',pais.name.official);
-            adicionarNodoContenedor(selectPaises,option);
-            
+            var option = document.createElement('option');
+            option.textContent = pais.name.official;
+            selectPaises.appendChild(option);
         }
-
     }
-
 }
 
-function mostrarDatosPaisSeleccionado(paisBuscar){
-
-    console.log(paisBuscar);
-
+function mostrarDatosPaisSeleccionado(paisBuscar) {
     var tabla = document.getElementById("tablaPaises");
 
-    tabla.remove();
+    tabla.innerHTML = ""; // Limpia la tabla antes de agregar nuevos datos
 
-   for (const pais of paises) {
+    for (const pais of respuestaPaisesAPI) {
+        if (paisBuscar === pais.name.official) {
+            var fila = tabla.insertRow(-1);
+            var colomnaNombre = fila.insertCell(0);
+            var colomnaCapital = fila.insertCell(1);
+            var colomnaRegion = fila.insertCell(2);
+            var columnacurrencies = fila.insertCell(3);
+            var colomnaBanderas = fila.insertCell(4);
+            var colomnaPoblacion = fila.insertCell(5);
 
-    if (paisBuscar == pais.name.common) {
-
-        var fila = tabla.insertRow(-1)
-        var columnaNombre = fila.insertCell(0)
-        var columnaCapital = fila.insertCell(1)
-        var columnaRegion = fila.insertCell(2)
-
-        columnaNombre.innerHTML = pais.name.official
-        columnaCapital.innerHTML = pais.capital[0]
-        columnaRegion.innerHTML = pais.region
-        
+            colomnaNombre.innerHTML = pais.name.official;
+            colomnaCapital.innerHTML = pais.capital[0];
+            colomnaRegion.innerHTML = pais.region;
+            columnacurrencies.innerHTML = obtenerNombresMonedas(pais.currencies);
+            colomnaBanderas.innerHTML = `<img src="${pais.flags.png}" alt="Bandera de ${pais.name.official}" width="50">`;
+            colomnaPoblacion.innerHTML = pais.population;
+        }
     }
-
-    
-   }
-
 }
--- js/utilidades.js
+++ js/utilidades.js
@@ -2,43 +2,43 @@ function crearNodo(tipoNodo){
 
     var nodo = document.createElement(tipoNodo);
     return nodo; 
-
+   
     // return documento.createElement(tipoNodo); este evita que hagamos un salto de linea con el return y lo pongamos todo en una sola linea 
-}
-
-
-function crearNodoTexto(tipoNodo, texto) {
-
+   }
+   
+   
+   function crearNodoTexto(tipoNodo, texto) {
+   
     var nodo = crearNodo(tipoNodo);
     var textoNodo = document.createTextNode(texto);
     nodo.appendChild(textoNodo);
     return nodo;
-
-}
-
-function crearNodoImagen(url , alt){
-
+   
+   }
+   
+   function crearNodoImagen(url , alt){
+   
     var nodo = crearNodo("img");
     nodo.src = url;
     nodo.alt = alt;
     return nodo;
-}
-
-function adicionarNodoContenedor(contenedor, nodo){
-
+   }
+   
+   function adicionarNodoContenedor(contenedor, nodo){
+   
     contenedor.appendChild(nodo);
-
-}
-
-function crearNodoA(texto, href){
-
+   
+   }
+   
+   function crearNodoA(texto, href){
+   
     var nodo = crearNodoTexto("a",texto);
     nodo.href = href;
     return nodo; 
-
-}
-
-function adicionarNodoBody(nodo){
+   
+   }
+   
+   function adicionarNodoBody(nodo){
     
     document.body.appendChild(nodo);
-}
+   }
